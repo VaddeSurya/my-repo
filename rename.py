960f66fa0a89e2c84f3cabe10f131e4459238ad0
Vadde Surya <22a91a12c0@aec.edu.in>
2023-12-08 15:39:00 +0530

    Add rename.py for project progress

--- rename.py
+++ rename.py
@@ -27,4 +27,4 @@ def rename_folders(base_path):
                 print(f"Renamed: {folder} -> {new_name}")
 rename_folders(base_path)
 
-// Commit timestamp: 2023-07-18 20:49:00
+// Commit timestamp: 2023-12-08 15:39:00
