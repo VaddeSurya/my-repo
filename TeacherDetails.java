2aaef9f6178ac37ff9ea02d91a4a823a83b9fedb
Vadde Surya <22a91a12c0@aec.edu.in>
2024-08-24 17:02:00 +0530

    Add TeacherDetails.java for project progress

---
+++ TeacherDetails.java
@@ -0,0 +1,7 @@
+package com.college.teacher.teacherdetails;
+public class TeacherDetails {
+    public void print(){
+        System.out.println("I am TeacherDetails");
+    }
+}
+// Commit timestamp: 2024-08-24 17:02:00
