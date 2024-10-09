2fc95b1363b7a5e6d341d34165e50276022fb192
Vadde Surya <22a91a12c0@aec.edu.in>
2024-10-09 16:42:00 +0530

    Add Product.java for project progress

--- Product.java
+++ Product.java
@@ -28,4 +28,4 @@ public class Product {
 //add the numbers in it and it gaves the value
 // IN int,float it return value which can you use in the
 //next main function
-// Commit timestamp: 2023-10-29 12:03:00
+// Commit timestamp: 2024-10-09 16:42:00
