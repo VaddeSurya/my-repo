c040feda167c3252fb14d47db1259e156b6497dc
Vadde Surya <22a91a12c0@aec.edu.in>
2025-04-07 09:34:00 +0530

    Add methods.java for project progress

---
+++ methods.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class methods {
+
+    public static void main(String[] args) {
+         String fullname = "HARSHJOSHI";
+    System.out.println(fullname.length());
+
+    }
+   
+}
+
+// Commit timestamp: 2025-04-07 09:34:00
