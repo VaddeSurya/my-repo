b079caaaa46b9c37354b5a06420776d40616d253
Vadde Surya <22a91a12c0@aec.edu.in>
2025-06-23 15:15:00 +0530

    Add MenMed.java for project progress

--- MenMed.java
+++ MenMed.java
@@ -44,4 +44,4 @@ public class MenMed {
 // this is one of the easy one okkey harsh
 // i have to just remenber the past ones
 // for the realizations
-// Commit timestamp: 2025-02-27 12:38:00
+// Commit timestamp: 2025-06-23 15:15:00
